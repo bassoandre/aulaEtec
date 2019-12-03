cc08b2b722109d9620eee173120d62acf1a3b78f
andrelpb <andrelpb@etehort.ps.sp>
2019-12-02 22:49:46 -0300

    Testes atualizado

--- aluno.php
+++ aluno.php
@@ -2,9 +2,26 @@
 	class Aluno
 	{
 		function media($a, $b, $c)
-		{
-			$media = $a * 0.2 + $b * 0.3 + $c * 0.5;				
-			return $media;			
+		{	
+			$a = str_replace(",",".",$a);
+			$b = str_replace(",",".",$b);
+			$c = str_replace(",",".",$c);
+			
+			if((!(is_numeric($a))) || (!(is_numeric($b))) || (!(is_numeric($c))))
+			{
+				return false;
+			}			
+
+			if(($a > 10) || ($b > 10) || ($c > 10) || ($a < 0) || ($b < 0) || ($c < 0))
+			{
+				return false;
+			}
+		
+			
+			$media = $a * 0.2 + $b * 0.3 + $c * 0.5;
+			$media = round($media,1);
+			return $media;
+					
 		}
 	}
 ?>
--- testes/testeAluno.php
+++ testes/testeAluno.php
@@ -13,6 +13,29 @@
 			$aluno = new Aluno();
 			//casos de teste da URI
 			$this->assertEqual($aluno->media(5.0,6.0,7.0),6.3);
+			$this->assertEqual($aluno->media(5.0,10.0,10.0),9.0);
+			$this->assertEqual($aluno->media(10.0,10.0,5.0),7.5);			
+			//teste de 0 a 10
+			$this->assertEqual($aluno->media(11.0,6.0,7.0),false);
+			$this->assertEqual($aluno->media(10.0,11.0,7.0),false);
+			$this->assertEqual($aluno->media(10.0,10.0,11.0),false);
+			$this->assertEqual($aluno->media(-1.0,6.0,7.0),false);
+			$this->assertEqual($aluno->media(1.0,-6.0,7.0),false);
+			$this->assertEqual($aluno->media(1.0,6.0,-7.0),false);
+			//letras
+			$this->assertEqual($aluno->media("A",6.0,5.0),false);
+			$this->assertEqual($aluno->media(6.0,"B",5.0),false);
+			$this->assertEqual($aluno->media(4.0,6.0,"C"),false);
+			//null
+			$this->assertEqual($aluno->media(4.0,6.0,""),false);
+			$this->assertEqual($aluno->media(4.0,6.0,null),false);
+			$this->assertEqual($aluno->media(4.0,null,5.0),false);
+			$this->assertEqual($aluno->media(null,4.0,5.0),false);
+			//quebrado
+			$this->assertEqual($aluno->media(6.44,3.0,10.0),7.2);
+			//teste com vírgula
+			$this->assertEqual($aluno->media("10,0","9,5","5,0"),7.4);
+			$this->assertEqual($aluno->media(10.0,9.5,5.0),7.4);
 		}
 	}
 
